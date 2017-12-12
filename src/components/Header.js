37d342d77617a78dc8382f8a282899d935c76a9c
Jason Crawford <daxapps777@gmail.com>
2017-12-11 22:41:58 -0600

    WIP question count

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import BackBtn from '../prev.jpg'
+import React, { Component } from 'react';
 
-const Header = (props) => {
-    if (props.isEnd())
-      return (<div></div>)
-    return (
-    <button className="back-btn" onClick={props.handleGoBack}>
-    	<img src={BackBtn} />
-    </button>
-    )
+import BackBtn from '../prev.jpg'
+import {data} from '../data/questions';
+
+class Header extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          currentQuestionId: 1,
+          questionList: data.questionData
+      }
+  }
+
+  render() {
+  	const {currentQuestionId, questionList} = this.state;
+	  if (this.props.isEnd())
+	     return (<div></div>)
+
+	  return (
+	  	<div>
+		    <button className="back-btn" onClick={this.props.handleGoBack}>
+		    	<img src={BackBtn} />
+		    </button>
+		    <div className="question-count">
+		      <span>Step {currentQuestionId} of {questionList.length}</span>
+		    </div>
+	    </div>
+	    )
+  }
+
 };
 
 export default Header;
