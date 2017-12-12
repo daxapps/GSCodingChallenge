7f9dd4b72f29ccfc2f3e3009c0b4c87b777a899d
Jason Crawford <daxapps777@gmail.com>
2017-12-11 21:46:53 -0600

    Improve UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import BackBtn from '../prev.jpg'
 
 const Header = (props) => {
     if (props.isEnd())
       return (<div></div>)
     return (
-    <button className="back-btn" onClick={props.handleGoBack}>Back</button>
+    <button className="back-btn" onClick={props.handleGoBack}>
+    	<img src={BackBtn} />
+    </button>
     )
 };
 
--- src/data/questions.js
+++ src/data/questions.js
@@ -8,9 +8,8 @@ export const data =
             "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
             "answers":
                 [
-                    {"id": 1, "answer": "answer11", "points": 2},
-                    {"id": 2, "answer": "answer12", "points": 1},
-                    {"id": 3, "answer": "answer13", "points": 0},
+                    {"id": 1, "answer": "Yes", "points": 2},
+                    {"id": 2, "answer": "No", "points": 1},
                 ]
             ,
             "selected": -1
@@ -21,9 +20,10 @@ export const data =
             "content": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
             "answers":
                 [
-                    {"id": 1, "answer": "answer21", "points": 2},
-                    {"id": 2, "answer": "answer22", "points": 1},
-                    {"id": 3, "answer": "answer23", "points": 0},
+                    {"id": 1, "answer": "Whatever!", "points": 2},
+                    {"id": 2, "answer": "OhK!!", "points": 1},
+                    {"id": 3, "answer": "Hmm...Maybe", "points": 0},
+                    {"id": 4, "answer": "Yeah!!!", "points": 0},
                 ]
             ,
             "selected": -1
@@ -37,6 +37,19 @@ export const data =
                     {"id": 1, "answer": "answer31", "points": 2},
                     {"id": 2, "answer": "answer32", "points": 1},
                     {"id": 3, "answer": "answer33", "points": 0},
+                ]
+            ,
+            "selected": -1
+        },
+        {
+            "id": 4,
+            "title": "Question 4",
+            "content": "This is the content for Question 4",
+            "answers":
+                [
+                    {"id": 1, "answer": "answer41", "points": 2},
+                    {"id": 2, "answer": "answer42", "points": 1},
+                    {"id": 3, "answer": "answer43", "points": 0},
                 ]
             ,
             "selected": -1
